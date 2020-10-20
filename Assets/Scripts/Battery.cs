6d56762d605bf2b09f24a6d782ec027acf933926
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-20 17:51:59 +0200

    added battery bar to UI

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField] private Slider battery = default;
+
+    void Start()
+    {
+        battery.value = 100; // fully loaded
+        InvokeRepeating("decreaseBattery", 1.0f, 1.0f);
+
+    }
+
+    void decreaseBattery(){
+        if(battery.value > 0) {
+            battery.value -= 1;
+        }
+    }
+    void Update()
+    {
+        battery.value -= 1; 
+    }
+}
