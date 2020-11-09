a93968f391ec9afec7571f44f623ebe8e6059616
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-11-09 18:04:05 +0100

    akkufresser, socker work, pop up does not

--- Assets/Scripts/Gameplay(not Dungeon)/WalkingCycle.cs
+++ Assets/Scripts/Gameplay(not Dungeon)/WalkingCycle.cs
@@ -14,7 +14,7 @@ public class WalkingCycle : MonoBehaviour
     // tile based movement 
     // https://www.youtube.com/watch?v=_Pm16a18zy8
     [SerializeField] private float moveSpeed = 5f;
-    //[SerializeField] private Rigidbody2D rb = default;
+    [SerializeField] private Rigidbody2D rb2D = default;
    // [SerializeField] private GameObject enemy;
     [SerializeField] private Animator animator  = default;
     [SerializeField] private Slider battery = default;
@@ -26,20 +26,27 @@ public class WalkingCycle : MonoBehaviour
      {
          Physics2D.IgnoreCollision(enemy.GetComponent<Collider2D>(), GetComponent<Collider2D>());
      }*/
+    private void Update()
+    {
+        // Input 
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            
+        // remove diagonal movement 
+        if (movement.x != 0)
+        {
+            movement.y = 0;
+        }
+        //Animation
+        animator.SetFloat("Horizontal", movement.x); //set Horizontal Parameter equal to the actual movement 
+        animator.SetFloat("Vertical", movement.y); //set Vertical Parameter equal to the actual movement 
+        animator.SetFloat("Speed", movement.SqrMagnitude()); // sqrt of abs of vector = speed     }
+    }
 
     void FixedUpdate()
     {
         if (!isMoving)
         {
-            // Input 
-            movement.x = Input.GetAxisRaw("Horizontal"); // number between -1 and 1
-            movement.y = Input.GetAxisRaw("Vertical"); // number between -1 and 1
-
-            // remove diagonal movement 
-            if (movement.x != 0)
-            {
-                movement.y = 0;
-            }
+            
             
             if (movement != Vector2.zero)
             {
@@ -49,12 +56,6 @@ public class WalkingCycle : MonoBehaviour
 
                 StartCoroutine(Move(targetPos));
             }
-            
-            //Animation
-            animator.SetFloat("Horizontal", movement.x); //set Horizontal Parameter equal to the actual movement 
-            animator.SetFloat("Vertical", movement.y); //set Vertical Parameter equal to the actual movement 
-            animator.SetFloat("Speed", movement.SqrMagnitude()); // sqrt of abs of vector = speed 
-
         }
     }
 
@@ -68,9 +69,11 @@ public class WalkingCycle : MonoBehaviour
         }
 
         transform.position = targetPos;
+        //rb2D.MovePosition(rb2D.position + movement * speed * Time.fixedDeltaTime);
         isMoving = false;
     }
 
+    //TODO extra script
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Akkufresser"))
